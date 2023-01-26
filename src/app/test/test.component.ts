7234549cdc174c0f3a8dc6705d99e7bb1ac3f00a
AyeshaAreej <ayeshaareej222@gmail.com>
2023-01-26 15:54:40 +0500

    interpolation added

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: ` <h2>{{ 'Welcome   ' + name }}</h2>
+    <h2>{{ 2 + 2 }}</h2>
+    <h2>{{ name.length }}</h2>
+    <h2>{{ name.toUpperCase() }}</h2>
+    <h2>{{ greetUser() }}</h2>
+    <h2>{{siteUrl}}</h2>`,
+  styles: [],
 })
-export class TestComponent {
-
+export class TestComponent implements OnInit {
+  //accessing global variable inside class due to unavailability in template
+  public siteUrl = window.location.href;
+  public name = 'Ayesha Areej';
+  constructor() {}
+  ngOnInit() {}
+  greetUser() {
+    return 'Hello  ' + this.name;
+  }
 }
